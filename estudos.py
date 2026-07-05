1f3e9855ae351bcabc01da36c8df6c4efed0f83b
rafaelnascimento-eng <nascimento.rafael.eng@gmail.com>
2026-07-05 13:29:38 -0300

    Delete estudos.py

--- estudos.py
+++
@@ -1,7 +0,0 @@
-medidor = {"id": 7742, "cliente": "Indústria ABC", "tensão_nominal": 380, "status": "Operacional"}
-
-medidor["status"] = "Manutenção"
-
-medidor["tecnico"] = "Rafael"
-
-print(medidor.get("localização", "Gaveta nao encontrada"))
